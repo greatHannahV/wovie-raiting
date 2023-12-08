ff5b7966d544cf21f1e7988519fe27e5ad1ef94e
greatHannahV <i.kasl@mail.ru>
2023-12-08 17:55:54 +0200

    added stars component, used api===props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import StarRating from 'StarRating.js'
 const tempMovieData = [
   {
     imdbID: 'tt1375666',
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+// import './index.css'
+// import App from './App'
+import StarRating from './StarRating'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={['terrible', 'bad', 'good', 'amazing', 'excellent']}
+    />
+    {/* <StarRating size={30} color="#999" className="test" dafaultRating={3} /> */}
   </React.StrictMode>,
 )
